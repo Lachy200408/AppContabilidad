7e042a3f967b48d037c11e772c7762c7e963d919
Lazaro-Laptop <Lazaro-Laptop@email.com>
2024-05-09 00:50:52 +0100

    XML file schema fixed

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,114 +1,153 @@
 window.onload = () => {
-  startForm();
-};
+  startForm()
+}
 
 function startForm() {
-  const form = document.querySelector("body>form");
+  const form = document.querySelector("body>form")
 
-  form.addEventListener("submit", submitForm, false);
+  form.addEventListener("submit", submitForm, false)
 
-  setListeners();
+  setListeners()
 }
 
 //* Funciones de handlers
 
 function submitForm(event) {
-  event.preventDefault();
-  const form = document.querySelector("body>form");
+  event.preventDefault()
+  const form = document.querySelector("body>form")
 
   //* Validar las fechas
-  const validacionFecha = validateDate(String(form.fecha.value));
+  const validacionFecha = validateDate(String(form.fecha.value))
   let errores = Object.values(validacionFecha).filter(
     (message) => message !== "ok"
-  );
+  )
   if (errores.length !== 0) {
-    displayModal(errores.join("\n"));
-    return;
+    displayModal(errores.join("\n"))
+    return
   }
-  const fecha = String(form.fecha.value);
+  const fecha = String(form.fecha.value)
 
   //* Validar el detalle
-  const validacionDetalle = validateDetail(form.detalle.value);
+  const validacionDetalle = validateDetail(form.detalle.value)
   errores = Object.values(validacionDetalle).filter(
     (message) => message !== "ok"
-  );
+  )
   if (errores.length !== 0) {
-    displayModal(errores.join("\n"));
-    return;
+    displayModal(errores.join("\n"))
+    return
   }
-  const detalle = form.detalle.value;
+  const detalle = form.detalle.value
 
   //* Validar las cuentas con las de la base de datos de la API
+
+	//* Tomar valores de cuentas
   const arrayCuentas = Array.from(
     document.querySelector("body>form>fieldset>ul").children
   ).map((li) => {
-    const liArray = Array.from(li.children);
-    const cuenta = liArray[0].value;
-    const debe = liArray[1].lastElementChild.value;
-    const haber = liArray[2].lastElementChild.value;
-    const arraySubcuentas = Array.from(liArray[3].children[1].children).map(
+    const liArray = Array.from(li.children)
+    const cuenta = liArray[0].value
+
+    const debe = '$' + parseFloat(liArray[1].lastElementChild.value).toFixed(2)
+    const haber = '$' + parseFloat(liArray[2].lastElementChild.value).toFixed(2)
+    
+		const arraySubcuentas = Array.from(liArray[3].children[1].children).map(
       (_li) => {
-        const subcuenta = _li.children[0].value;
-        const parcial = _li.children[1].firstElementChild.value;
+        const subcuenta = _li.children[0].value
+        const parcial = '$' + parseFloat(_li.children[1].firstElementChild.value).toFixed(2)
 
         return {
           subcuenta: subcuenta,
           parcial: parcial,
-        };
+        }
       }
-    );
+    )
 
     return {
       cuenta: cuenta,
       debe: debe,
       haber: haber,
       subcuentas: arraySubcuentas,
-    };
-  });
+    }
+  })
 
   //* Exportar el registro
-  const xmlArrayCuentas = arrayCuentas
-    .map((_cuenta) => {
-      const xmlSubcuentas = _cuenta.subcuentas
-        .map((_subcuenta) => {
-          return `<subcuenta>
-								<nombre>${_subcuenta.subcuenta}</nombre>
-								<parcial>${_subcuenta.parcial}</parcial>
-							</subcuenta>`;
-        })
-        .join("");
+  let xmlCuentas = []
+	arrayCuentas.forEach((_cuenta) => {
+		const nombre = (_cuenta.debe !== '$NaN')? _cuenta.cuenta : '    '+_cuenta.cuenta
 
-      return `<cuenta>
-							<nombre>${_cuenta.cuenta}</nombre>
-							<debe>${_cuenta.debe}</debe>
-							<haber>${_cuenta.haber}</haber>
-							${xmlSubcuentas}
-						</cuenta>`;
+		xmlCuentas.push(`<Cuentas_Detalles>${nombre}</Cuentas_Detalles>`)
+		
+		_cuenta.subcuentas.forEach(_subcuenta => {
+			xmlCuentas.push((_cuenta.debe !== '$NaN')?
+								`<Cuentas_Detalles>  ${_subcuenta.subcuenta}</Cuentas_Detalles>` :
+								`<Cuentas_Detalles>      ${_subcuenta.subcuenta}</Cuentas_Detalles>`)
+		})
+	})
+
+	let xmlParcial = []
+  arrayCuentas.forEach((_cuenta) => {
+		xmlParcial.push(`<Parcial></Parcial>`)
+		
+		
+		_cuenta.subcuentas.forEach(_subcuenta => {
+			xmlParcial.push(`<Parcial>${_subcuenta.parcial}</Parcial>`)
+		})
+	})
+
+	let xmlDebe = []
+	arrayCuentas.forEach((_cuenta) => {
+			if (_cuenta.debe === '$NaN') {
+				xmlDebe.push(`<Debe></Debe>`)
+			}
+			else {
+				xmlDebe.push(`<Debe>${_cuenta.debe}</Debe>`)
+			}
+
+      _cuenta.subcuentas.forEach(_subcuenta => {
+				xmlDebe.push(`<Debe></Debe>`)
+			})
     })
-    .join("");
 
-  const xml = `<?xml version="1.0" encoding="utf-8"?>
-								<tabla>
-									<fecha>${fecha}</fecha>
-									${xmlArrayCuentas}
-									<detalle>${detalle}</detalle>
-								</tabla>`;
+  let xmlHaber = []
+	arrayCuentas.forEach((_cuenta) => {
+		if (_cuenta.haber === '$NaN') {
+			xmlHaber.push(`<Haber></Haber>`)
+		}
+		else {
+			xmlHaber.push(`<Haber>${_cuenta.haber}</Haber>`)
+		}
 
-  console.log("Llega aqui");
-  downloadReg(xml);
+		_cuenta.subcuentas.forEach(_subcuenta => {
+			xmlHaber.push(`<Haber></Haber>`)
+		})
+	})
+
+	let xmlBody = ''
+	for(let i=0; i < xmlCuentas.length+1; i++){
+		const xmlFecha = (i === 0)? `<Fecha>${fecha}</Fecha>` : `<Fecha></Fecha>`
+		const primeros = xmlFecha + xmlCuentas[i] + xmlParcial[i] + xmlDebe[i] + xmlHaber[i]
+		const ultimo = xmlFecha + `<Cuentas_Detalles>${detalle}</Cuentas_Detalles><Parcial></Parcial><Debe></Debe><Haber></Haber>`
+
+		if (i !== xmlCuentas.length) xmlBody += `<Registro>${primeros}</Registro>`
+		else xmlBody += `<Registro>${ultimo}</Registro>`
+	}
+
+  const xml = 
+	`<?xml version="1.0" encoding="utf-8"?>\n <Tabla>\n ${xmlBody}\n </Tabla>`
+
+  downloadReg(xml)
 }
 
 function modifyCuentas(event) {
-  event.preventDefault();
-  console.log(event.target);
+  event.preventDefault()
 
-  const isCuenta = !String(event.target.id).includes("Sub");
-  const itemKind = isCuenta ? "cuenta" : "subcuenta";
+  const isCuenta = !String(event.target.id).includes("Sub")
+  const itemKind = isCuenta ? "cuenta" : "subcuenta"
 
   //* Selecciono el elemento correcto dependiendo del boton
   const list = isCuenta
     ? document.querySelector("body>form>fieldset>ul")
-    : event.target.parentElement.children[1];
+    : event.target.parentElement.children[1]
 
   //* Html de los items
   const htmlCuenta = `
@@ -160,7 +199,7 @@ function modifyCuentas(event) {
 							-
 						</button>
 					</details>
-	`;
+	`
   const htmlSubCuenta = `
 		<input type="text" list="subcuentas" required class="form-control"/>
 
@@ -168,49 +207,41 @@ function modifyCuentas(event) {
 			Parcial: 
 			<input type="number" name="parcial" id="parcial" required class="form-control"/>
 		</label>
-	`;
-  const html = document.createElement("li");
-  html.className = itemKind + " list-unstyled";
-  html.innerHTML = isCuenta ? htmlCuenta : htmlSubCuenta;
+	`
+  const html = document.createElement("li")
+  html.className = itemKind + " list-unstyled"
+  html.innerHTML = isCuenta ? htmlCuenta : htmlSubCuenta
 
   if (event.target.innerText === "+") {
-    list.appendChild(html);
+    list.appendChild(html)
   } else {
-    const length = list.children.length;
+    const length = list.children.length
 
     //* Desactivar boton cuando queden dos cuentas y 0 subcuentas
-    if ((length === 2 && isCuenta) || (length === 0 && !isCuenta)) return;
+    if ((length === 2 && isCuenta) || (length === 0 && !isCuenta)) return
 
-    list.children[length - 1].remove();
+    list.children[length - 1].remove()
   }
 
   //* Resetear los listeners
-  resetListeners();
+  resetListeners()
 }
 
+let valor = ''
 function handlerBalances(event) {
-  const input = event.target;
-
-  if (event.type === "change") {
-    if (
-      input.value !== "" &&
-      !input.value.includes(",") &&
-      !input.value.includes(".")
-    )
-      input.value = String(input.value).concat(",00");
-  } else {
-    //* Validar entrada
-    if (input.value === "") input.value = "";
-  }
-
-  //* Resetear el contiguo
-  let element = input.parentElement;
-  if (element.innerText === "Parcial: ") return;
-  element =
-    element.innerText === "Acreditando:"
-      ? element.previousElementSibling
-      : element.nextElementSibling;
-  element = element.lastElementChild.value = "";
+  const input = event.target  
+	
+	//* Validar entrada
+	if (input.value === "" && valor.length !== 1) input.value = valor
+	valor = input.value
+	//* Resetear el contiguo
+	let element = input.parentElement
+	if (element.innerText === "Parcial:") return
+	element =
+		element.innerText === "Acreditando:"
+			? element.previousElementSibling
+			: element.nextElementSibling
+	element = element.lastElementChild.value = ""
 }
 
 //* Funciones de listeners
@@ -218,53 +249,53 @@ function handlerBalances(event) {
 function setListeners() {
   //* Colocar listeners de botones
   document.querySelectorAll("fieldset button").forEach((btn) => {
-    btn.addEventListener("click", modifyCuentas, false);
-  });
+    btn.addEventListener("click", modifyCuentas, false)
+  })
   //* Colocar listeners de inputs numericos
   document.querySelectorAll('input[type="number"]').forEach((input) => {
-    input.addEventListener("input", handlerBalances, false);
-    input.addEventListener("change", handlerBalances, false);
-  });
+    input.addEventListener("input", handlerBalances, false)
+    input.addEventListener("change", handlerBalances, false)
+  })
 }
 
 function removeListeners() {
   //* Quitar listeners de botones
   document.querySelectorAll("fieldset button").forEach((btn) => {
-    btn.removeEventListener("click", modifyCuentas);
-  });
+    btn.removeEventListener("click", modifyCuentas)
+  })
   //* Quitar listeners de inputs numericos
   document.querySelectorAll('input[type="number"]').forEach((input) => {
-    input.removeEventListener("input", handlerBalances);
-  });
+    input.removeEventListener("input", handlerBalances)
+  })
 }
 
 function resetListeners() {
-  removeListeners();
-  setListeners();
+  removeListeners()
+  setListeners()
 }
 
 //* Funciones de validacion
 
 function validateDate(fecha) {
-  const ymd = fecha.split("-").map((num) => parseInt(num));
+  const ymd = fecha.split("-").map((num) => parseInt(num))
   const fechaActual = {
     year: new Date().getFullYear(),
     month: new Date().getMonth() + 1,
     day: new Date().getDate(),
-  };
+  }
   const resultado = {
     year: "",
     month: "",
     day: "",
-  };
+  }
 
   if (ymd[0] < 2000 || ymd[0] > fechaActual.year)
-    resultado.year = "El año está incorrecto";
-  else resultado.year = "ok";
+    resultado.year = "El año está incorrecto"
+  else resultado.year = "ok"
 
   if (ymd[1] > 12 || ymd[1] < 1 || ymd[1] > fechaActual.month)
-    resultado.month = "El mes está incorrecto";
-  else resultado.month = "ok";
+    resultado.month = "El mes está incorrecto"
+  else resultado.month = "ok"
 
   if (
     ymd[2] > 31 ||
@@ -279,50 +310,48 @@ function validateDate(fecha) {
     (ymd[2] > 30 && ymd[1] !== 12) ||
     ymd[2] > fechaActual.day
   )
-    resultado.day = "El día está incorrecto";
-  else resultado.day = "ok";
+    resultado.day = "El día está incorrecto"
+  else resultado.day = "ok"
 
-  return resultado;
+  return resultado
 }
 
 function validateDetail(detalle) {
   const resultado = {
     malRedaccion: "",
     inconsistencia: "",
-  };
+  }
 
-  if (detalle.includes("egistrando")) resultado.malRedaccion = "ok";
+  if (detalle.includes("egistrando")) resultado.malRedaccion = "ok"
   else
     resultado.malRedaccion =
-      'Los detalles están escritos incorrectamente. Debe iniciar con "Registrando".';
+      'Los detalles están escritos incorrectamente. Debe iniciar con "Registrando".'
 
-  if (detalle.split(" ").length > 5) resultado.inconsistencia = "ok";
-  else resultado.inconsistencia = "Argumenta un poco más los detalles.";
+  if (detalle.split(" ").length > 5) resultado.inconsistencia = "ok"
+  else resultado.inconsistencia = "Argumenta un poco más los detalles."
 
-  return resultado;
+  return resultado
 }
 
 function displayModal(text) {
-  alert("Ha ocurrido un problema en su formulario:\n" + text);
+  alert("Ha ocurrido un problema en su formulario:\n" + text)
 }
 
 //* Funciones de exportacion
 
-let cantReg = 1;
+let cantReg = 1
 function downloadReg(text, fileName = "Sin-titulo") {
-  const blob = new Blob([text], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
+  const blob = new Blob([text], { type: "text/plain" })
+  const url = URL.createObjectURL(blob)
 
-  console.log(text);
+  const a = document.createElement("a")
+  a.href = url
+  a.download = `${fileName}${cantReg++}.xml`
+  a.style.display = "none"
 
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = `${fileName}${cantReg++}.xml`;
-  a.style.display = "none";
+  document.body.appendChild(a)
+  a.click()
 
-  document.body.appendChild(a);
-  a.click();
-
-  document.body.removeChild(a);
-  URL.revokeObjectURL(url);
+  document.body.removeChild(a)
+  URL.revokeObjectURL(url)
 }
